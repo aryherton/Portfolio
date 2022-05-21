faba6578baedf2cee30d3612e9f01cdc5f0d1196
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-21 16:27:48 -0300

    feat: created "BrowserRouter" in file "index.js"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle, theme } from './styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <GlobalStyle />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
