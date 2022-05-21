c6d5886393f106f19157095e998d509d1de78bbd
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-21 16:27:11 -0300

    feat: created "Route Home" in file "App.js"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+    </Routes>
   );
 }
 
