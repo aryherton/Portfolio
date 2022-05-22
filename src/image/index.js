104b3cee666d292c105920e3e60db21496828bc7
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-22 11:58:33 -0300

    feat:  created file "./src/image/index.js"

---
+++ src/image/index.js
@@ -0,0 +1,13 @@
+import myLogo from './my-logo.png';
+import myPhoto from './my-not-background.png';
+import linkedinIcon from './linkedin_32px.ico';
+import githubIcon from './github_32px.ico';
+import instaIcon from './instagram_32px.ico';
+
+export {
+  myLogo,
+  myPhoto,
+  linkedinIcon,
+  githubIcon,
+  instaIcon,
+}
