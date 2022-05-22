9956e561fe945ceaa9addff40f6ccc96e48d3308
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-22 08:28:00 -0300

    feat: import file "HomeWrapper, HeaderWrapper"

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,6 +1,10 @@
-import { GlobalStyle, theme } from './globlaStyle';
+import { GlobalStyle, theme } from './globalStyle';
+import { HomeWrapper } from './home';
+import { HeaderWrapper } from './header';
 
 export {
   GlobalStyle,
   theme,
+  HomeWrapper,
+  HeaderWrapper,
 }
