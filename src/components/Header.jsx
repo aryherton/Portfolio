969bca67e330160a3cad4fd6faf1a3808380285b
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-22 11:58:58 -0300

    feat: inserted new icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,26 @@ import React from "react";
 
 import { HeaderWrapper } from "../styles";
 
-import my_img from '../image/my-not-background.png';
+import {
+  myLogo,
+  myPhoto,
+  linkedinIcon,
+  githubIcon,
+  instaIcon
+} from '../image';
 
 export default function Header() {
   return (
     <HeaderWrapper>
       <div className="divMainHeader">
         <div className="myImageHeader">
-          <img src={ my_img } alt="my_image" />
-          <hr />
+          <img src={ myLogo } alt="my_logo" id="myLogo" />
+          <img src={ myPhoto } alt="my_photo" id="myPhoto"/>
+          <div id="socialMediaHeader">
+            <img src={ linkedinIcon } alt="Linkedin_Icon" />
+            <img src={ githubIcon } alt="Github_Icon" />
+            <img src={ instaIcon } alt="Instagram_Icon" />
+          </div>
         </div>
       </div>
     </HeaderWrapper>
