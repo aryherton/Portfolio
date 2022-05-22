2a06a33079f7a2f5b5e082a370ca36b2ad92f3ae
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-22 08:22:45 -0300

    feat: import component "HomeWrapper"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
+import { HomeWrapper } from '../styles';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function Home() {
   return (
-    <>
+    <HomeWrapper>
       <Header />
       <Footer />
-    </>
+    </HomeWrapper>
   )
 }
