da24abf641502e7319e7ac646e6a949ba758b245
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-22 12:01:16 -0300

    feat: inserted css "img#myLogo" in file "./src/styles/header.js"

--- src/styles/header.js
+++ src/styles/header.js
@@ -5,7 +5,7 @@ export const HeaderWrapper = style.header`
   align-items: center;
   justify-content: center;
   position: relative;
-  height: 50%;
+  height: 40%;
   width: 100%;
   background-color: ${props => props.theme.colors.primary};
 
@@ -17,6 +17,7 @@ export const HeaderWrapper = style.header`
   }
 
   div.myImageHeader {
+    position: relative;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -25,14 +26,35 @@ export const HeaderWrapper = style.header`
     background-color: ${props => props.theme.colors.primary};
     border-bottom: 4px solid ${props => props.theme.colors.secondary};
 
-    img {
+    img#myLogo {
+      width: 200px;
+      height: 80px;
       position: absolute;
-      top: 29%;
+      left: 15%;
+      top: 40%;
+      background-color: ${props => props.theme.colors.primary};
+    }
+
+    img#myPhoto {
+      position: absolute;
+      top: 45%;
       border-radius: 50%;
       border: 3px solid ${props => props.theme.colors.secondary};
-      height: 250px;
-      width: 250px;
+      height: 200px;
+      width: 200px;
       object-fit: cover;
+    }
+
+    div#socialMediaHeader {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      position: absolute;
+      right: 2%;
+      height: 110px;
+      width: 25px;
+      bottom: 5%;
+      background-color: ${props => props.theme.colors.secondary};
     }
   }
 `;
