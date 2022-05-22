186896d082056c885eee40febd30c5ebce3dbe75
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-22 08:21:06 -0300

    feat: created style component "HeaderWrapper"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
+import { HeaderWrapper } from "../styles";
+
+import my_img from '../image/my-not-background.png';
+
 export default function Header() {
   return (
-    <>
-      <header>
-        <h1>Header</h1>
-      </header>
-    </>
+    <HeaderWrapper>
+      <div className="divMainHeader">
+        <div className="myImageHeader">
+          <img src={ my_img } alt="my_image" />
+          <hr />
+        </div>
+      </div>
+    </HeaderWrapper>
   )
 }
