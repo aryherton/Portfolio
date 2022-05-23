7f80c410116a95e91119da672fb9b42a44a086c8
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-22 22:08:34 -0300

    feat: inserted "import" in file "./src/styles/index.js"

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,10 +1,16 @@
 import { GlobalStyle, theme } from './globalStyle';
 import { HomeWrapper } from './home';
 import { HeaderWrapper } from './header';
+import { AboutWrapper } from './about';
+import { MenuHomeTop } from './menuHomeTop';
+import { FooterWrapper } from './footer';
 
 export {
   GlobalStyle,
   theme,
   HomeWrapper,
   HeaderWrapper,
+  AboutWrapper,
+  MenuHomeTop,
+  FooterWrapper,
 }
