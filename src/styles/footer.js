9390aac0aebea5193af39368fa3530c043901a30
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-22 22:05:45 -0300

    feat: create file "./src/styles/footer.js"

---
+++ src/styles/footer.js
@@ -0,0 +1,11 @@
+import style from 'styled-components';
+
+export const FooterWrapper = style.footer`
+  width: 100%;
+  height: 30%;
+  background-color: gray;
+
+  h1 {
+    background-color: transparent;
+  }
+`;
