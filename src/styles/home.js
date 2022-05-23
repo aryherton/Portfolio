d7456e977d651813e2977fcd4addf1b1ff8cafae
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-22 22:07:58 -0300

    fix: change structure css in file "./src/styles/home.js"

--- src/styles/home.js
+++ src/styles/home.js
@@ -5,15 +5,14 @@ export const HomeWrapper = style.div`
   margin: 5px auto;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  height: 98vh;
-  width: 99vw;
+  width: 98%;
   background-color: ${props => props.theme.colors.primary};
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.55);
 
   div#menuSuper {
     position: relative;
     width: 100%;
-    height: 4%;
+    height: 30px;
     background-color: gray;
 
     ul {
@@ -30,5 +29,13 @@ export const HomeWrapper = style.div`
         width: 100%;
       }
     }
+  }
+
+  main {
+    position: relative;
+    z-index: 1;
+    width: 100%;
+    height: 400px;
+    background-color: transparent;
   }
 `;
