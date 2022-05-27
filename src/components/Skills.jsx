53bb0c43f8cd929b899101097868d17446be9f94
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-27 10:08:44 -0300

    fix: change structure "ul" inserted "map()"

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { SkillsWrapper } from '../styles';
 import { checkMark2 } from '../image';
+import arrSkill from '../help/tecnologSkill.js';
 
 export default function Skills() {
   return (
@@ -10,100 +11,28 @@ export default function Skills() {
         <h1>Habilidade em:</h1>
         <div id="divSkills">
           <ul id="firstUl">
-            <li>
-              <img src={ checkMark2 } alt="check_mark_green" />
-              HTML5
-            </li>
-            <li>
-              <img src={ checkMark2 } alt="check_mark_green" />
-                CSS
-              </li>
-            <li>
-            <img src={ checkMark2 } alt="check_mark_green" />
-              Tailwind
-            </li>
-            <li>
-            <img src={ checkMark2 } alt="check_mark_green" />
-              Style-Components
-            </li>
-            <li>
-              <img src={ checkMark2 } alt="check_mark_green" />
-              JavaScript
-            </li>
-            <li>
-            <img src={ checkMark2 } alt="check_mark_green" />
-              Typescript
-            </li>
-            <li>
-            <img src={ checkMark2 } alt="check_mark_green" />
-              React
-            </li>
-            <li>
-            <img src={ checkMark2 } alt="check_mark_green" />
-              MySQL
-            </li>
-            <li>
-            <img src={ checkMark2 } alt="check_mark_green" />
-              SQL
-            </li>
-            <li>
-            <img src={ checkMark2 } alt="check_mark_green" />
-              Sequelize
-            </li>
-            <li>
-              <img src={ checkMark2 } alt="check_mark_green" />
-              TDD
-            </li>
+            { arrSkill.map((item, index) => {
+              if (index < 11) {
+                return (<li key={ index }>
+                        <img src={ checkMark2 } alt="check_mark_green" />
+                        { item }
+                      </li>)
+              }
+              return null;
+            })
+            }
           </ul>
           <ul id="secondUl">
-            <li>
-            <img src={ checkMark2 } alt="check_mark_green" />
-              Jest
-            </li>
-            <li>
-            <img src={ checkMark2 } alt="check_mark_green" />
-              Mocha
-            </li>
-            <li>
-              <img src={ checkMark2 } alt="check_mark_green" />
-              Chai
-            </li>
-            <li>
-              <img src={ checkMark2 } alt="check_mark_green" />
-              Sinon
-            </li>
-            <li>
-              <img src={ checkMark2 } alt="check_mark_green" />
-              React Test Library
-            </li>
-            <li>
-              <img src={ checkMark2 } alt="check_mark_green" />
-              Redux
-            </li>
-            <li>
-              <img src={ checkMark2 } alt="check_mark_green" />
-              Git
-            </li>
-            <li>
-              <img src={ checkMark2 } alt="check_mark_green" />
-              GitHub
-            </li>
-            <li>
-              <img src={ checkMark2 } alt="check_mark_green" />
-              Docker
-            </li>
-            <li>
-              <img src={ checkMark2 } alt="check_mark_green" />
-              Heroku
-            </li>
-            <li>
-              <img src={ checkMark2 } alt="check_mark_green" />
-              NodeJS
-            </li>
-            <li>
-              <img src={ checkMark2 } alt="check_mark_green" />
-              Express
-            </li>
+          { arrSkill.map((item, index) => {
+              if (index > 10) {
+                return (<li key={ index }>
+                        <img src={ checkMark2 } alt="check_mark_green" />
+                        { item }
+                      </li>)
+              }
+              return null;
+            })
+            }
           </ul>
         </div>
       </div>
