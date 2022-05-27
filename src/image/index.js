8eaee0ed73585ea165a9a87f8dfbf30efe469f29
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-26 21:30:23 -0300

    feat: import and export "checkMark2, instaFooter, gitHub32, linkedin32"

--- src/image/index.js
+++ src/image/index.js
@@ -2,7 +2,11 @@ import myLogo from './my-logo.png';
 import myPhoto from './my-not-background.png';
 import linkedinIcon from './linkedin_32px.ico';
 import githubIcon from './github_32px.ico';
-import instaIcon from './instagram_32px.ico';
+import instaIcon from './instagram_32.ico';
+import checkMark2 from './check-mark2.png';
+import instaFooter from './instagram32px.png';
+import gitHub32 from './github32px.png';
+import linkedin32 from './linkedin32.png';
 
 export {
   myLogo,
@@ -10,4 +14,8 @@ export {
   linkedinIcon,
   githubIcon,
   instaIcon,
+  checkMark2,
+  instaFooter,
+  gitHub32,
+  linkedin32
 }
