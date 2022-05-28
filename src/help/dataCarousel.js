bfef11d8e2203221e8618ab71180babc0490e112
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-27 22:41:12 -0300

    feat: created file "./src/help/dataCarousel.js"

---
+++ src/help/dataCarousel.js
@@ -0,0 +1,46 @@
+import { projectTrivia } from '../image';
+
+import { trello, redux, reactPng } from '../image';
+
+export const arrImg = [
+  {
+    img: projectTrivia,
+    alt: 'projectTrivia',
+    title: 'Project Trivia',
+    tecnology: [reactPng, trello, redux],
+    describe: `
+      Projeto em grupo visando colocar em pratica "Soft-Skills".
+    `,
+    link: 'https://trivia-ajustes.vercel.app/',
+  },
+  {
+    img: projectTrivia,
+    alt: 'projectTrivia',
+    title: 'Project Trivia',
+    tecnology: [reactPng, trello, redux],
+    describe: `
+      Projeto em grupo visando colocar em pratica "Soft-Skills".
+    `,
+    link: 'https://trivia-ajustes.vercel.app/',
+  },
+  {
+    img: projectTrivia,
+    alt: 'projectTrivia',
+    title: 'Project Trivia',
+    tecnology: [reactPng, trello, redux],
+    describe: `
+      Projeto em grupo visando colocar em pratica "Soft-Skills".
+    `,
+    link: 'https://trivia-ajustes.vercel.app/',
+  },
+  {
+    img: projectTrivia,
+    alt: 'projectTrivia',
+    title: 'Project Trivia',
+    tecnology: [reactPng, trello, redux],
+    describe: `
+      Projeto em grupo visando colocar em pratica "Soft-Skills".
+    `,
+    link: 'https://trivia-ajustes.vercel.app/',
+  },
+];
