1de4f5a4c0fd059777ef4a74e4975ea4e966b6eb
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-27 22:47:52 -0300

    fix: change styled "section" for "div"

--- src/styles/about.js
+++ src/styles/about.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const AboutWrapper = styled.section`
+export const AboutWrapper = styled.div`
   position: relative;
   width: 100%;
   height: 250px;
