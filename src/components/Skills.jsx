36feabbd981fad9cc0876ffa404e0c5c6f2df744
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-27 22:39:51 -0300

    refactor: created new component "UlSkills"

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,41 +1,12 @@
 import React from 'react';
 
 import { SkillsWrapper } from '../styles';
-import { checkMark2 } from '../image';
-import arrSkill from '../help/tecnologSkill.js';
+import UlSkills from './UlSkills';
 
 export default function Skills() {
   return (
     <SkillsWrapper>
-      <div id="divMainSkills">
-        <h1>Habilidade em:</h1>
-        <div id="divSkills">
-          <ul id="firstUl">
-            { arrSkill.map((item, index) => {
-              if (index < 11) {
-                return (<li key={ index }>
-                        <img src={ checkMark2 } alt="check_mark_green" />
-                        { item }
-                      </li>)
-              }
-              return null;
-            })
-            }
-          </ul>
-          <ul id="secondUl">
-          { arrSkill.map((item, index) => {
-              if (index > 10) {
-                return (<li key={ index }>
-                        <img src={ checkMark2 } alt="check_mark_green" />
-                        { item }
-                      </li>)
-              }
-              return null;
-            })
-            }
-          </ul>
-        </div>
-      </div>
+      <UlSkills />
     </SkillsWrapper>
   );
 }
